c9074b541989093a6c3caaf8c845e0ae5ae26eeb
Ilya Emelyanov <emel.9616@gmail.com>
2019-05-20 11:42:52 +0700

    Depl-144456 Initial implementation
    update startIntegration.py

--- startIntegration.py
+++ startIntegration.py
@@ -3,7 +3,7 @@ import json
 
 with open('SettingsFileTemplate.integration.json', "rb") as PFile:
     passwordData = json.loads(PFile.read().decode('utf-8'))
-    
+
 urlUpsource = passwordData["urlUpsource"]
 loginUpsource = passwordData["loginUpsource"]
 passUpsource = passwordData["passUpsource"]
@@ -14,4 +14,7 @@ passOnevizion = passwordData["passOnevizion"]
 projectName = passwordData["projectName"]
 projectOnevizion = passwordData["projectOnevizion"]
 
-upsourceIntegration.integration(urlUpsource=urlUpsource, loginUpsource=loginUpsource, passUpsource=passUpsource, urlOnevizion=urlOnevizion, loginOnevizion=loginOnevizion, passOnevizion=passOnevizion, projectName=projectName, projectOnevizion=projectOnevizion)
+with open('ihub_process_id', "rb") as PFile:
+    processId = PFile.read().decode('utf-8')
+
+upsourceIntegration.integration(urlUpsource=urlUpsource, loginUpsource=loginUpsource, passUpsource=passUpsource, urlOnevizion=urlOnevizion, loginOnevizion=loginOnevizion, passOnevizion=passOnevizion, projectName=projectName, projectOnevizion=projectOnevizion, processId=processId)
