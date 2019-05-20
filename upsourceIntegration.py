19d99eab708dbd8b4c7285347b9cb968cf131637
Ilya Emelyanov <emel.9616@gmail.com>
2019-05-20 10:44:59 +0700

    Depl-144456 Initial implementation
    update upsourceIntegration.py

--- upsourceIntegration.py
+++ upsourceIntegration.py
@@ -143,6 +143,7 @@ class integration(object):
     
     #Returns logging to stdout
     def get_logger(self, name=__file__, file='log.txt', encoding='utf-8'):
+        print(5, flush = True)
         log = logging.getLogger(name)
         log.setLevel(logging.DEBUG)
         formatter = logging.Formatter('[%(asctime)s] %(filename)s:%(lineno)d %(levelname)-8s %(message)s')
