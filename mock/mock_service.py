4a8f2b88c2f4835ec988573c8201f7e51c84d486
P4radise <emel.9616@gmail.com>
2021-04-17 00:53:44 +0700

    IHub-173327 Update the integration to work with the mock service

--- mock/mock_service.py
+++ mock/mock_service.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, make_response
-from flask_wtf.csrf import CSRFProtect
 from functools import wraps
 from enum import Enum
 
@@ -7,9 +6,6 @@ import json
 
 
 app = Flask(__name__)
-
-csrf = CSRFProtect(app)
-app.config['WTF_CSRF_ENABLED'] = False
 
 def auth_required(f):
     @wraps(f) 
@@ -26,25 +22,20 @@ def auth_required(f):
 
     return decorated
 
-@app.route('/')
-@auth_required
-def main():
-    return 'You are logged in'
-
 @app.route('/~rpc/getRevisionsListFiltered',  methods=['POST'])
 @auth_required
 def get_filtered_revision_list():
     query = request.get_json()['query']
 
-    if query not in (Issue.ID_TEST_2.value, Issue.ID_TEST_1.value):
+    if query not in (Issue.TEST_ISSUE_2.issue_id, Issue.TEST_ISSUE_1.issue_id):
         return make_response(f'Revision for {query} not found', StatusCode.EXCEPTION.value)
 
-    if query == Issue.ID_TEST_2.value:
-        json_data = {'result': {'revision': [{'revisionId': Review.ID_TEST_2.value, 
-                'revisionCommitMessage': f'{Issue.ID_TEST_2.value} {Issue.SUMMARY_TEST_2.value}\n'}]}}
-    elif query == Issue.ID_TEST_1.value:
-        json_data = {'result': {'revision': [{'revisionId': Review.ID_TEST_1.value, 
-                'revisionCommitMessage': f'{Issue.ID_TEST_1.value} {Issue.SUMMARY_TEST_1.value}\n'}]}}
+    if query == Issue.TEST_ISSUE_2.issue_id:
+        json_data = {'result': {'revision': [{'revisionId': Review.TEST_REVIEW_2.review_id, 
+                'revisionCommitMessage': f'{Issue.TEST_ISSUE_2.issue_id} {Issue.TEST_ISSUE_2.summary}\n'}]}}
+    elif query == Issue.TEST_ISSUE_1.issue_id:
+        json_data = {'result': {'revision': [{'revisionId': Review.TEST_REVIEW_1.review_id, 
+                'revisionCommitMessage': f'{Issue.TEST_ISSUE_1.issue_id} {Issue.TEST_ISSUE_1.summary}\n'}]}}
     else:
         json_data = {'result': {'query': f'{query}'}}
 
@@ -55,7 +46,7 @@ def get_filtered_revision_list():
 def close_review():
     review_id = request.get_json()['reviewId']['reviewId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
     return review_id
@@ -65,10 +56,10 @@ def close_review():
 def get_branch():
     query = request.get_json()['query']
 
-    if query == Issue.ID_TEST_2.value:
+    if query == Issue.TEST_ISSUE_2.issue_id:
         json_data =  {'result': {'defaultBranch': 'master'}}
-    elif query == Issue.ID_TEST_1.value:
-        json_data = {'result': {'branch': [{'name': Issue.ID_TEST_1.value}]}}
+    elif query == Issue.TEST_ISSUE_1.issue_id:
+        json_data = {'result': {'branch': [{'name': Issue.TEST_ISSUE_1.issue_id}]}}
     else:
         return make_response(f'Branch {query} not found', StatusCode.EXCEPTION.value)
 
@@ -79,7 +70,7 @@ def get_branch():
 def start_branch_tracking():
     review_id = request.get_json()['reviewId']['reviewId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
     return review_id
@@ -89,12 +80,12 @@ def start_branch_tracking():
 def find_users():
     pattern = request.get_json()['pattern']
 
-    if pattern == User.NAME_TEST_1.value:
-        json_data = {'result': {'infos': [{'userId': User.ID_TEST_1.value}]}}
-    elif pattern == User.NAME_TEST_2.value:
-        json_data = {'result': {'infos': [{'userId': User.ID_TEST_2.value}]}}
-    elif pattern == User.NAME_TEST_3.value:
-        json_data = {'result': {'infos': [{'userId': User.ID_TEST_3.value}]}}
+    if pattern == User.TEST_USER_1.user_name:
+        json_data = {'result': {'infos': [{'userId': User.TEST_USER_1.user_id}]}}
+    elif pattern == User.TEST_USER_2.user_name:
+        json_data = {'result': {'infos': [{'userId': User.TEST_USER_2.user_id}]}}
+    elif pattern == User.TEST_USER_3.user_name:
+        json_data = {'result': {'infos': [{'userId': User.TEST_USER_3.user_id}]}}
     else:
         return make_response(f'Cannot resolve pattern {pattern}', StatusCode.EXCEPTION.value)
 
@@ -106,10 +97,10 @@ def update_participant_status():
     review_id = request.get_json()['reviewId']['reviewId']
     user_id = request.get_json()['userId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
-    if user_id not in (User.ID_TEST_1.value, User.ID_TEST_2.value, User.ID_TEST_3.value):
+    if user_id not in (User.TEST_USER_1.user_id, User.TEST_USER_2.user_id, User.TEST_USER_3.user_id):
         return make_response(f'Cannot resolve user id {user_id}', StatusCode.EXCEPTION.value)
 
     return user_id
@@ -120,10 +111,10 @@ def add_reviewer():
     review_id = request.get_json()['reviewId']['reviewId']
     user_id = request.get_json()['participant']['userId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
-    if user_id not in (User.ID_TEST_1.value, User.ID_TEST_2.value, User.ID_TEST_3.value):
+    if user_id not in (User.TEST_USER_1.user_id, User.TEST_USER_2.user_id, User.TEST_USER_3.user_id):
         return make_response(f'Cannot resolve user id {user_id}', StatusCode.EXCEPTION.value)
 
     return user_id
@@ -134,10 +125,10 @@ def remove_reviewer():
     review_id = request.get_json()['reviewId']['reviewId']
     user_id = request.get_json()['participant']['userId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
-    if user_id not in (User.ID_TEST_1.value, User.ID_TEST_2.value, User.ID_TEST_3.value):
+    if user_id not in (User.TEST_USER_1.user_id, User.TEST_USER_2.user_id, User.TEST_USER_3.user_id):
         return make_response(f'Cannot resolve user id {user_id}', StatusCode.EXCEPTION.value)
 
     return user_id
@@ -148,36 +139,35 @@ def get_reviews():
     with open('settings.json', 'rb') as PFile:
         settings_url = json.loads(PFile.read().decode('utf-8'))['urlOneVizion']
 
-    REVIEW_JSON_DATA_1 = {'reviewId':{'projectId':'blank','reviewId':Review.SUMMARY_TEST_1.value},
-                          'title':f'{Issue.ID_TEST_1.value} {Issue.SUMMARY_TEST_1.value}',
-                          'description':f'[{Issue.ID_TEST_1.value}-64]({settings_url}trackor_types/Issue_Task/trackors.do?key={Issue.ID_TEST_1.value}-64) amoiseenko\n \
-                                          [{Issue.ID_TEST_1.value}-62]({settings_url}trackor_types/Issue_Task/trackors.do?key={Issue.ID_TEST_1.value}-62) vadim.glebov',
-                          'participants':[{'userId':User.ID_TEST_3.value,'role':2,'state':2},
-                                          {'userId':User.ID_TEST_1.value,'role':2,'state':2},
-                                          {'userId':User.ID_TEST_2.value,'role':2,'state':2},
-                                          {'userId':User.ID_TEST_4.value,'role':1,'state':2}],
-                          'state':1,'createdBy':User.ID_TEST_3.value}
-    REVIEW_JSON_DATA_2 = {'reviewId':{'projectId':'blank','reviewId':Review.SUMMARY_TEST_2.value},
-                          'title':f'{Issue.ID_TEST_2.value} {Issue.SUMMARY_TEST_2.value}',
-                          'participants':[{'userId':User.ID_TEST_1.value,'role':1,'state':1}],
-                          'state':1,'createdBy':User.ID_TEST_3.value}
+    REVIEW_JSON_DATA_1 = {'reviewId':{'projectId':'blank','reviewId':Review.TEST_REVIEW_1.review_key},
+                          'title':f'{Issue.TEST_ISSUE_1.issue_id} {Issue.TEST_ISSUE_1.summary}',
+                          'description':f'[{Issue.TEST_ISSUE_1.issue_id}-64]({settings_url}trackor_types/Issue_Task/trackors.do?key={Issue.TEST_ISSUE_1.issue_id}-64) amoiseenko\n \
+                                          [{Issue.TEST_ISSUE_1.issue_id}-62]({settings_url}trackor_types/Issue_Task/trackors.do?key={Issue.TEST_ISSUE_1.issue_id}-62) vadim.glebov',
+                          'participants':[{'userId':User.TEST_USER_3.user_id,'role':2,'state':2},
+                                          {'userId':User.TEST_USER_1.user_id,'role':2,'state':2},
+                                          {'userId':User.TEST_USER_2.user_id,'role':2,'state':2},
+                                          {'userId':User.TEST_USER_4.user_id,'role':1,'state':2}],
+                          'state':1,'createdBy':User.TEST_USER_3.user_id}
+    REVIEW_JSON_DATA_2 = {'reviewId':{'projectId':'blank','reviewId':Review.TEST_REVIEW_2.review_key},
+                          'title':f'{Issue.TEST_ISSUE_2.issue_id} {Issue.TEST_ISSUE_2.summary}',
+                          'participants':[{'userId':User.TEST_USER_1.user_id,'role':1,'state':1}],
+                          'state':1,'createdBy':User.TEST_USER_3.user_id}
   
 
     query = request.get_json()['query']
 
-    if query not in (Issue.ID_TEST_2.value, Issue.ID_TEST_1.value, Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value, 'state: open'):
+    if query not in (Issue.TEST_ISSUE_2.issue_id, Issue.TEST_ISSUE_1.issue_id, Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key, 'state: open'):
         return make_response(f'Review for {query} not found', StatusCode.EXCEPTION.value)
 
 
-    if query == Issue.ID_TEST_2.value:
+    if query == Issue.TEST_ISSUE_2.issue_id:
         json_data = {'result': {'hasMore': False, 'totalCount': 0}}
-    elif query == Review.SUMMARY_TEST_2.value:
+    elif query == Review.TEST_REVIEW_2.review_key:
         json_data = {'result':{'reviews':[REVIEW_JSON_DATA_2]}}
-    elif query == Issue.ID_TEST_1.value or query == Review.SUMMARY_TEST_1.value:
+    elif query in (Issue.TEST_ISSUE_1.issue_id, Review.TEST_REVIEW_1.review_key):
         json_data = {'result':{'reviews':[REVIEW_JSON_DATA_1]}}
     elif query == 'state: open':
         json_data = {'result':{'reviews':[REVIEW_JSON_DATA_1, REVIEW_JSON_DATA_2]}}
-        print(json_data)
     else:
         json_data = {'result': {'hasMore': False, 'totalCount': 0}}
 
@@ -188,7 +178,7 @@ def get_reviews():
 def rename_review():
     review_id = request.get_json()['reviewId']['reviewId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
     return review_id
@@ -198,16 +188,16 @@ def rename_review():
 def create_review():
     revisions = request.get_json()['revisions']
 
-    if revisions == Review.ID_TEST_2.value:
+    if revisions == Review.TEST_REVIEW_2.review_id:
         json_data = {'result':{
-                'reviewId':{'projectId':'blank','reviewId':Review.SUMMARY_TEST_2.value},
-                'title':f'{Issue.ID_TEST_2.value} {Issue.SUMMARY_TEST_2.value}',
-                'participants':[{'userId':User.ID_TEST_3.value,'role':2,'state':1},
-                                {'userId':User.ID_TEST_1.value,'role':1,'state':1}],
-                'state':1,'createdBy':User.ID_TEST_3.value}}
-    elif revisions == Review.ID_TEST_1.value:
-        return make_response(f'Cannot create review because revision {revisions} is already in review Review(reviewId=ReviewId[{Review.ID_TEST_1.value}], \
-                                title=\'{Issue.ID_TEST_1.value} {Issue.SUMMARY_TEST_1.value}\'', StatusCode.EXCEPTION.value)
+                'reviewId':{'projectId':'blank','reviewId':Review.TEST_REVIEW_2.review_key},
+                'title':f'{Issue.TEST_ISSUE_2.issue_id} {Issue.TEST_ISSUE_2.summary}',
+                'participants':[{'userId':User.TEST_USER_3.user_id,'role':2,'state':1},
+                                {'userId':User.TEST_USER_1.user_id,'role':1,'state':1}],
+                'state':1,'createdBy':User.TEST_USER_3.user_id}}
+    elif revisions == Review.TEST_REVIEW_1.review_id:
+        return make_response(f'Cannot create review because revision {revisions} is already in review Review(reviewId=ReviewId[{Review.TEST_REVIEW_1.review_id}], \
+                                title=\'{Issue.TEST_ISSUE_1.issue_id} {Issue.TEST_ISSUE_1.summary}\'', StatusCode.EXCEPTION.value)
     else:
         return make_response(f'Cannot resolve revision {revisions} in project blank', StatusCode.EXCEPTION.value)
 
@@ -218,7 +208,7 @@ def create_review():
 def update_review_description():
     review_id = request.get_json()['reviewId']['reviewId']
 
-    if review_id not in (Review.SUMMARY_TEST_1.value, Review.SUMMARY_TEST_2.value):
+    if review_id not in (Review.TEST_REVIEW_1.review_key, Review.TEST_REVIEW_2.review_key):
         return make_response(f'Review {review_id} not found', StatusCode.EXCEPTION.value)
 
     return review_id
@@ -230,30 +220,35 @@ class StatusCode(Enum):
 
 
 class Review(Enum):
-    ID_TEST_1 = '6c3fd124ae40d499e7c27d22381c8a07e8f78afa'
-    ID_TEST_2 = '5de06f7e6562b385e707b9beee3c559d1a237184'
 
-    SUMMARY_TEST_1 = 'BLNK-CR-127'
-    SUMMARY_TEST_2 = 'BLNK-CR-128'
+    def __init__(self, review_id, review_key):
+        self.review_id = review_id
+        self.review_key = review_key
+
+    TEST_REVIEW_1 = ('6c3fd124ae40d499e7c27d22381c8a07e8f78afa', 'BLNK-CR-127')
+    TEST_REVIEW_2 = ('5de06f7e6562b385e707b9beee3c559d1a237184', 'BLNK-CR-128')
 
 
 class Issue(Enum):
-    ID_TEST_1 = 'Depl-125306'
-    ID_TEST_2 = 'IHub-146144'
 
-    SUMMARY_TEST_1 = 'Configure \'blank\' project in AWS'
-    SUMMARY_TEST_2 = 'Test Upsource integration'
+    def __init__(self, issue_id, summary):
+        self.issue_id = issue_id
+        self.summary = summary
+
+    TEST_ISSUE_1 = ('Depl-125306', 'Configure \'blank\' project in AWS')
+    TEST_ISSUE_2 = ('IHub-146144', 'Test Upsource integration')
 
 
 class User(Enum):
-    ID_TEST_1 = '4006a614-5f72-4c75-a6e7-389e49ccb4fa'
-    ID_TEST_2 = 'dfed774e-37c7-49bd-9059-6aa51bad1bb8'
-    ID_TEST_3 = '30c91c19-e69f-4de9-92a1-7da68aec71af'
-    ID_TEST_4 = '795d4839-3e99-4b76-b35c-e70f80ab856c'
 
-    NAME_TEST_1 = 'asmoiseenko'
-    NAME_TEST_2 = 'Vadim'
-    NAME_TEST_3 = 'Ilya Emelyanov'
+    def __init__(self, user_id, user_name):
+        self.user_id = user_id
+        self.user_name = user_name
+
+    TEST_USER_1 = ('4006a614-5f72-4c75-a6e7-389e49ccb4fa', 'asmoiseenko')
+    TEST_USER_2 = ('dfed774e-37c7-49bd-9059-6aa51bad1bb8', 'Vadim')
+    TEST_USER_3 = ('30c91c19-e69f-4de9-92a1-7da68aec71af', 'Ilya Emelyanov')
+    TEST_USER_4 = ('795d4839-3e99-4b76-b35c-e70f80ab856c', '')
 
 
 if __name__ == '__main__':
